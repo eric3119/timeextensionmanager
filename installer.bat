0cbab1c2b0aaa2400677b726ebeb7653262a2833
eric3119 <esc2@ic.ufal.br>
2024-12-08 11:08:29 -0300

    Update installer.bat to support multiple username parameters for scheduled tasks

--- installer.bat
+++ installer.bat
@@ -6,9 +6,9 @@ curl https://codeload.github.com/eric3119/timeextensionmanager/tar.gz/main -o ti
 tar -xvf timeextensionmanager.tar.gz
 
 :: create scheduled task run on logon
-schtasks /create /tn "TimeExtensionManager" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py check %1" /sc onlogon /ru %1 /F
+schtasks /create /tn "TimeExtensionManager" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py check %*" /sc onlogon /ru %* /F
 
 :: create scheduled task run every 60 minutes replace the old one
-schtasks /create /tn "TimeExtensionManager60min" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py fetch %1" /sc minute /mo 60 /ru %1 /F
+schtasks /create /tn "TimeExtensionManager60min" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py fetch %*" /sc minute /mo 60 /ru %* /F
 
-cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py check %1
+cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py check %*
