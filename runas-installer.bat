6ef883978d49dd30e9142045ab1298b5ff678f7c
eric3119 <esc2@ic.ufal.br>
2024-12-08 14:31:52 -0300

    Add password prompt to runas-installer.bat for secure execution of installer.bat

--- runas-installer.bat
+++ runas-installer.bat
@@ -1,1 +1,6 @@
-powershell "start \"installer.bat\" \"%1\"" -v runAs"
+@echo off
+set "psCommand=powershell -Command "$pword = read-host 'Enter Password' -AsSecureString ; ^
+    $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+        [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set password=%%p
+powershell "start \"installer.bat\" \"%password%\"" -v runAs"
