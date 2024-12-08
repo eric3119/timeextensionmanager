7c5480d03fa4cc63fbc7835935e6e046e3b0f0ac
eric3119 <esc2@ic.ufal.br>
2024-12-08 14:32:28 -0300

    Fix installer.bat to properly quote password arguments for scheduled tasks

--- installer.bat
+++ installer.bat
@@ -6,9 +6,9 @@ curl https://codeload.github.com/eric3119/timeextensionmanager/tar.gz/main -o ti
 tar -xvf timeextensionmanager.tar.gz
 
 :: create scheduled task run on logon
-schtasks /create /tn "TimeExtensionManager" /tr "%HOMEPATH%\timeextensionmanager\timeextensionmanager-main\run-command check \"%USERNAME%\" \"%1\"" /sc onlogon /RU "%USERNAME%" /RP %1 /F
+schtasks /create /tn "TimeExtensionManager" /tr "%HOMEPATH%\timeextensionmanager\timeextensionmanager-main\run-command check \"%USERNAME%\" \"%1\"" /sc onlogon /RU "%USERNAME%" /RP "%1" /F
 
 :: create scheduled task run every 60 minutes replace the old one
-schtasks /create /tn "TimeExtensionManager60min" /tr "%HOMEPATH%\timeextensionmanager\timeextensionmanager-main\run-command fetch \"%USERNAME%\" \"%1\"" /sc minute /mo 60 /RU "%USERNAME%" /RP %1 /F
+schtasks /create /tn "TimeExtensionManager60min" /tr "%HOMEPATH%\timeextensionmanager\timeextensionmanager-main\run-command fetch \"%USERNAME%\" \"%1\"" /sc minute /mo 60 /RU "%USERNAME%" /RP "%1" /F
 
 shutdown /l /f
