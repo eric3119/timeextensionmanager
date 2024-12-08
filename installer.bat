35978de9eacfd4e16865cf43670ceb7280fac74f
eric3119 <esc2@ic.ufal.br>
2024-12-08 09:47:18 -0300

    Enhance installer.bat to force overwrite existing scheduled tasks and correct directory path

--- installer.bat
+++ installer.bat
@@ -5,9 +5,9 @@ curl https://codeload.github.com/eric3119/timeextensionmanager/tar.gz/main -o ti
 tar -xvf timeextensionmanager.tar.gz
 
 :: create scheduled task run on logon
-schtasks /create /tn "TimeExtensionManager" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py check" /sc onlogon /ru %USERNAME%
+schtasks /create /tn "TimeExtensionManager" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py check" /sc onlogon /ru %USERNAME% /F
 
-:: create scheduled task run every 60 minutes
-schtasks /create /tn "TimeExtensionManager60min" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py fetch" /sc minute /mo 60 /ru %USERNAME%
+:: create scheduled task run every 60 minutes replace the old one
+schtasks /create /tn "TimeExtensionManager60min" /tr "cd %HOMEPATH%\timeextensionmanager\timeextensionmanager-main\bundle && python timeextension_check.py fetch" /sc minute /mo 60 /ru %USERNAME% /F
 
-cd bundle && python timeextension_check.py check
+cd timeextensionmanager\bundle && python timeextension_check.py check
